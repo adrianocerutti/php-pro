cb1ce59e6581b47af7b9d116cbc3441f3a6bf948
Adriano Cerutti <adrianocerutti@gmail.com>
2022-09-26 15:10:39 -0300

    Pegando as rotas dinâmicas

--- app/router/router.php
+++ app/router/router.php
@@ -1,24 +1,42 @@
 <?php
 
-function routes() {
-    
+function routes()
+{
+
     return require 'routes.php';
 }
 
-function exactMatchUriInArrayRoutes($uri, $routes) {
+function exactMatchUriInArrayRoutes($uri, $routes)
+{
 
-    if(array_key_exists($uri, $routes)) {
-        return [];
+    if (array_key_exists($uri, $routes)) {
+        return [$uri => $routes[$uri]];
     }
 
     return [];
 }
 
-function router() {
+function regularExpressionMatchArrayRoutes($uri, $routes)
+{
+    array_filter(
+        $routes,
+        function ($value) use ($uri) {
+            $regex = str_replace('/', '\/', ltrim($value, '/'));
+            return preg_match("/^$regex$/", ltrim($uri, '/'));
+        },
+        ARRAY_FILTER_USE_KEY
+    );
+}
 
+function router()
+{
     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-    
+
     $routes = routes();
 
     $matchedUri = exactMatchUriInArrayRoutes($uri, $routes);
+
+    if (empty($matchedUri)) {
+        $matchedUri = regularExpressionMatchArrayRoutes($uri, $routes);
+    }
 }
